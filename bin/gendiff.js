97e200d00d3e7db802572489716f2fa0990ba2e7
pollyleyka <polka321@rambler.ru>
2022-11-30 13:20:03 +0400

    add dev dep eslint and test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import gendiff from '../src/index.js'
+import gendiff from '../src/index.js';
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.1.0')
-    .argument('<filePath1>')
-    .argument('<filePath1>')
-    .helpOption(true, 'output usage information')
-    .option('-f, --format < type >', 'output format', )
-    .action(gendiff);
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0')
+  .argument('<filePath1>')
+  .argument('<filePath1>')
+  .helpOption(true, 'output usage information')
+  .option('-f, --format < type >', 'output format')
+  .action(gendiff);
 
 program.parse();
-
-
