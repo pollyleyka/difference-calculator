95e5406d4214b9566f15636ea707ba3d775c7274
pollyleyka <polka321@rambler.ru>
2022-12-04 14:29:32 +0400

    fixed node.yml for action, fixed test file

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,6 +1,12 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import gendiff from '../src/index.js';
 import expected from '../__fixtures__/expected.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 test('gendiff', () => {
-  expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expected);
+  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected);
 });
