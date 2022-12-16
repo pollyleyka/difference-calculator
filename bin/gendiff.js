f6232e3a3222d853d725b0278d11757353ac48e1
pollyleyka <polka321@rambler.ru>
2022-12-16 21:41:19 +0400

    create a local Command object to use

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
 import gendiff from '../src/index.js';
 
+const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
