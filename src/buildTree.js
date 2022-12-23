e04df30bafc3fac1c40a3e0df7e23a7a00fde870
pollyleyka <polka321@rambler.ru>
2022-12-23 21:41:35 +0400

    fixed builder fn

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,23 +1,6 @@
 import _ from 'lodash';
 
-const isObject = (data) => data !== null && typeof data === 'object' && !Array.isArray(data);
-
-const isEqual = (data1, data2) => {
-  if (Array.isArray(data1) && Array.isArray(data2)) {
-    if (data1.length !== data2.length) {
-      return false;
-    }
-    const sortedData1 = data1.sort();
-    const sortedData2 = data2.sort();
-    for (let i = 0; i < sortedData1.length; i += 1) {
-      if (sortedData1[i] !== sortedData2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return data1 === data2;
-};
+const isObject = (data) => _.isObject(data) && !Array.isArray(data);
 
 const buildTree = (data1, data2) => {
   const keys2 = Object.keys(data1);
@@ -49,7 +32,7 @@ const buildTree = (data1, data2) => {
         };
       }
 
-      if (!isEqual(data1[key], data2[key])) {
+      if (!_.isEqual(data1[key], data2[key])) {
         return {
           key,
           type: 'changed',
